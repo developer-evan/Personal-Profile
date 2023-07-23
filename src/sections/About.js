c152f1faf0c2b08adf13fdecf16e0445843d4a3e
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-07-23 22:47:30 +0300

    implemented about section

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,39 +1,50 @@
 import React from "react";
-import { FaArrowRight, FaCheckCircle } from "react-icons/fa";
+import { FaArrowRight} from "react-icons/fa";
 
 function About() {
   return (
-    <div className="flex justify-around  bg-slate-300">
-      
-      <div className="text-start w-1/2">
-      <h1 className="text-sky-600 text-4xl p-2">About</h1>
-        <p>
-          Well, I’ve been working on web app development for two years now,
-          while still studying at college. Having my own projects, coded mostly
-          in Node JS and React JS, I have learned a lot while working on these
-          projects. Now I am looking to get an internship to get some
-          experience, and am glad to be interviewing with you here.
-        </p>
-        <div className="mt-4">
-          <p>
-            These are some qualities that I think sets me apart from other
-            candidates
+    <div className="flex justify-center items-center h-screen bg-slate-300">
+      <div className="w-full max-w-5xl p-6 bg-slate-300 rounded-lg shadow-md flex flex-wrap">
+        <div className="w-full md:w-1/2 md:pr-6">
+          <h1 className="text-sky-600 text-4xl">About</h1>
+          <div className="flex justify-between items-center mb-4">
+            
+            <img
+              src="./assets/abouu.jpg"
+              alt="about"
+              width={350}
+              height={350}
+              className="md:hidden rounded-md shadow-md"
+            />
+          </div>
+          <p className="text-gray-800 text-start">
+            Welcome! My name is Evans Mogeni, and I am passionate about web app
+            development. I have been immersed in this dynamic field for the past
+            two years, all while pursuing my studies in college. During this time,
+            I have embarked on various projects, with a primary focus on Node JS
+            and React JS technologies. These experiences have been invaluable,
+            enriching my skill set and deepening my understanding of web
+            development.
           </p>
-          <p className="flex">
-            <FaCheckCircle className="text-sky-600 text-4xl" /> One of the
-            things that distinguish me from other candidates is that I am able
-            to remain calm and focused no matter how difficult the task. I
-            always try to stay positive and find a way out. I am sure that
-            everything happens for a reason and that any mistake can be fixed.
-          </p>
-          {/* <p><FaCheckCircle/></p>
-            <p><FaCheckCircle/></p>
-            <p><FaCheckCircle/></p> */}
+          <div className="mt-4">
+            <p className="font-semibold text-gray-800">
+              What sets me apart from other candidates:
+            </p>
+                       
+          </div>
+          <button className="bg-sky-600 flex items-center text-white p-2 mt-6">
+            Check out my Projects <FaArrowRight className="ml-2" />
+          </button>
         </div>
-        <button className="bg-sky-600 flex items-center text-white  p-2 justify-between mt-3">Check my Projects  <FaArrowRight/></button>
-      </div>
-      <div className="p-4">
-        <img src="./assets/abouu.jpg" alt="about" width={350} height={350} />
+        <div className="hidden md:block md:w-1/2">
+          <img
+            src="./assets/abouu.jpg"
+            alt="about"
+            width={350}
+            height={350}
+            className="rounded-md shadow-md"
+          />
+        </div>
       </div>
     </div>
   );
